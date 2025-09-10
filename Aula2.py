2ebb136cb057b2f390805ca9fe1e5eb19a60392f
Gian Lucas <gianserafim.c@gmail.com>
2025-09-10 13:32:22 -0300

    aula2

---
+++ Aula2.py
@@ -0,0 +1,14 @@
+
+# Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+#print(len(input("Digite seu nome: ")))
+
+#Exercício 02
+#Criar um programa onde o usuário digite dois valores e apareça a soma
+#print(int(input("Digite um numero: ")) + int(input("Digite um numero: ")))
+
+#pagina de exercicios python https://www.w3schools.com/python/python_exercises.asp
+
+#Exercício 03: Refatore o exercício 02 atribuindo variáveis
+#nome = input("Digite seu nome: ")
+#quantidade_caracters = len(nome)
+#print(quantidade_caracters)
